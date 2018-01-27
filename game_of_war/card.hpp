5745dcb77c8085e05d58c8fb5bbe5593512cedbe
kangere <kangere721@gmail.com>
2018-01-27 16:40:06 -0500

    Adding game of war project

---
+++ game_of_war/card.hpp
@@ -0,0 +1,27 @@
+#ifndef CARD_HPP
+#define CARD_HPP
+
+enum Rank {
+	Ace,
+	Two,
+	Three,
+	Four,
+	Five,
+	Six,
+	Seven,
+	Eight,
+	Nine,
+	Ten,
+	Jack,
+	Queen,
+	King,
+};
+
+enum Suit {
+	Hearts,
+	Diamonds,
+	Clubs,
+	Spades,
+};
+
+#endif

a74a9230fae109056643c53f02c6c1671a66016b
kangere <kangere721@gmail.com>
2018-03-23 09:57:38 -0400

    Card class implemented using union

---
+++ game_of_war/union_card.hpp
@@ -0,0 +1,106 @@
+#ifndef	UNION_CARD_HPP
+#define UNION_CARD_HPP
+
+enum Rank {
+	Ace,
+	Two,
+	Three,
+	Four,
+	Five,
+	Six,
+	Seven,
+	Eight,
+	Nine,
+	Ten,
+	Jack,
+	Queen,
+	King,
+};
+
+enum Suit {
+	Hearts,
+	Diamonds,
+	Clubs,
+	Spades,
+};
+
+enum Color 
+{
+	Black,
+	Red
+};
+
+
+class StandardCard
+{
+
+private:
+	Rank rank;
+	Suit suit;
+
+public:
+	StandardCard(Rank rank, Suit suit)
+	: rank(rank), suit(suit) {}
+	
+	Rank get_rank() const {return rank;}
+	Suit get_suit() const { return suit;}	
+
+};
+
+class JokerCard
+{
+private:
+	Color color;
+
+public:
+	JokerCard(Color color): color(color)
+	{}
+
+	Color get_color() const {return color;}
+
+};
+
+class Card
+{
+public:
+	enum Kind
+	{
+		standard_card,
+		joker_card
+	};
+
+private:
+	union Value
+	{
+		StandardCard sc;
+		JokerCard jc;
+
+		Value(StandardCard sc): sc(sc) {}
+		Value(JokerCard jc): jc(jc) {}
+	};
+
+	Kind kind;
+	Value val;
+
+public:
+	Card(StandardCard c): kind(standard_card), val(c)
+	{}
+
+	Card(JokerCard c): kind(joker_card), val(c)
+	{}
+
+	Rank get_rank () const;
+	Suit get_suit () const;
+
+	Color get_color () const;
+
+	bool is_standard()
+	{return kind == standard_card ? true : false;}
+
+	bool is_joker()
+	{return kind == joker_card ? true : false;}
+};
+
+
+
+#endif

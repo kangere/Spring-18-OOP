ff019e033bf1588aab7ad4ca42bc275f9c10453d
kangere <kangere721@gmail.com>
2018-04-01 00:10:10 -0400

    Testing bitcard

--- game_of_war/main.cpp
+++ game_of_war/main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <cassert>
 // #include "card.hpp"
-/*#include "bitCard.hpp"
-*/
-#include "union_card.hpp"
+#include "bitCard.hpp"
+
+// #include "union_card.hpp"
 int main()
 {
+	Card c(Ace,Spades);
+	assert(c.is_standard());
 
+	Card c2(Red);
+	assert(c2.is_joker());
+	/* union card tests
 	StandardCard sc{Ace,Spades};
 	Card c{sc};
+	Card jc{JokerCard(Red)};
 
 	assert(c.is_standard());
+	std::cout << "Asser card is_standard working" << std::endl;
+	assert(jc.is_joker());
+	std::cout << "Asser card is_joker working" << std::endl;*/
 	 
 	return 0;
 }

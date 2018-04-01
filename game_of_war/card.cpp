cdf18e588df5634a35b6e1aa916eed63bc08e30e
kangere <kangere721@gmail.com>
2018-04-01 18:49:16 -0400

    Created card class with inheritance

--- game_of_war/card.cpp
+++ game_of_war/card.cpp
@@ -1,1 +1,17 @@
-#include card.hpp
+#include "card.hpp"
+#include <iostream>
+
+
+std::ostream& operator<<(std::ostream& os, const StandardCard& card)
+{
+	os << rank_name[card.rank] << " " << suit_name[card.suit];
+
+	return os;
+}
+
+std::ostream& operator<<(std::ostream &os, const JokerCard &card )
+{
+	os << color_name[card.color] ;
+
+	return os;
+}
--- game_of_war/card.hpp
+++ game_of_war/card.hpp
@@ -1,6 +1,43 @@
 #ifndef CARD_HPP
 #define CARD_HPP
 
+#define stringify(name) #name
+
+#include <iosfwd>
+
+/*Card with inheritance*/
+const static char *rank_name[] =
+{
+	stringify(Ace),
+	stringify(Two),
+	stringify(Three),
+	stringify(Four),
+	stringify(Five),
+	stringify(Six),
+	stringify(Seven),
+	stringify(Eight),
+	stringify(Nine),
+	stringify(Ten),
+	stringify(Jack),
+	stringify(Queen),
+	stringify(King)
+}; 
+
+const static char *suit_name[] = 
+{
+	stringify(Hearts),
+	stringify(Diamonds),
+	stringify(Clubs),
+	stringify(Spades)
+};
+
+const static char *color_name[] = 
+{
+	stringify(Black),
+	stringify(Red)
+};
+
+
 enum Rank {
 	Ace,
 	Two,
@@ -17,6 +54,7 @@ enum Rank {
 	King,
 };
 
+
 enum Suit {
 	Hearts,
 	Diamonds,
@@ -24,14 +62,50 @@ enum Suit {
 	Spades,
 };
 
+
+
+enum Color {
+	Black,
+	Red
+};
+
+
+
 class Card
 {
+private:
+	
+
+public:
+	Card() {}
+};
+
+class StandardCard : public Card {
 private:
 	Rank rank;
 	Suit suit;
 
 public:
-	Card(Rank r, Suit s):rank(r), suit(s){}
+	StandardCard(Rank r, Suit s):
+	rank(r), suit(s) {}
+
+	Rank get_rank() const {return rank;}
+
+	friend std::ostream& operator<<(std::ostream&, const StandardCard&); 
+
 };
+
+class JokerCard: public Card {
+private:
+	Color color;
+
+public:
+	JokerCard(Color c): 
+	color(c) {}
+
+	friend std::ostream& operator<<(std::ostream&,const JokerCard&);
+
+};
+
 
 #endif

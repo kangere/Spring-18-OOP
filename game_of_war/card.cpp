a377ab10a29144997c24cd8e366f8d2aa8b1d381
kangere <kangere721@gmail.com>
2018-04-03 18:38:08 -0400

    Added Deck data structure, not yet tested

--- game_of_war/card.cpp
+++ game_of_war/card.cpp
@@ -1,7 +1,25 @@
 #include "card.hpp"
 #include <iostream>
 
+Deck::Deck(const Deck& deck)
+{
+	for(Card *c: deck)
+	{
+		Card new = c->clone();
+		*this.push_back(&new);
+	}
 
+}
+
+StandardCard StandardCard::clone()
+{
+	return new StandardCard(this->get_rank(),this->get_suit());
+}
+
+JokerCard JokerCard::clone()
+{
+	return new JokerCard(this->get_color());
+}
 std::ostream& operator<<(std::ostream& os, const StandardCard& card)
 {
 	os << rank_name[card.rank] << " " << suit_name[card.suit];
--- game_of_war/card.hpp
+++ game_of_war/card.hpp
@@ -4,6 +4,7 @@
 #define stringify(name) #name
 
 #include <iosfwd>
+#include <vector>
 
 /*Card with inheritance*/
 const static char *rank_name[] =
@@ -78,6 +79,7 @@ private:
 
 public:
 	Card() {}
+	virtual Card clone() = 0;
 };
 
 class StandardCard : public Card {
@@ -91,6 +93,8 @@ public:
 
 	Rank get_rank() const {return rank;}
 
+	StandardCard clone();
+
 	friend std::ostream& operator<<(std::ostream&, const StandardCard&); 
 
 };
@@ -103,8 +107,23 @@ public:
 	JokerCard(Color c): 
 	color(c) {}
 
+	JokerCard clone();
 	friend std::ostream& operator<<(std::ostream&,const JokerCard&);
 
+};
+
+class Deck: public std::vector<Card*>{
+public:
+	Deck(const Deck&);
+
+	Deck(std::initializer_list<Card*> list):
+	std::vector<Card*>(list) {}
+
+	~Deck()
+	{
+		for(Card *c: *this)
+			delete c;
+	}
 };
 
 
